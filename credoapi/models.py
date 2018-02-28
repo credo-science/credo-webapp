1a298792343f800a70555fe5077629241303c297
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-03-01 00:09:31 +0100

    Add models.py contents.

--- credoapi/models.py
+++ credoapi/models.py
@@ -3,4 +3,38 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+
+# TODO: integrate with Django's user?
+class User(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    email = models.CharField(max_length=255)
+    name = models.CharField(max_length=24)
+    registerDate = models.DateTimeField()
+    key = models.CharField(max_length=255)
+
+
+# TODO: do we need this?
+class Device(models.Model):
+    deviceId = models.CharField(max_length=255)
+    deviceModel = models.CharField(max_length=255)
+    androidVersion = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Detection(models.Model):
+    accuracy = models.FloatField()
+    altitude = models.FloatField()
+    frame_content = models.BinaryField()
+    height = models.IntegerField()
+    width = models.IntegerField()
+    id = models.IntegerField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    provider = models.CharField(max_length=24)
+    timestamp = models.DateTimeField()
