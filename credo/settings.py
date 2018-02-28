5f5dd575a9d446b26cb975274c931817577fbbb8
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-03-01 00:21:12 +0100

    Enable credoapi app.

--- credo/settings.py
+++ credo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'credoapi.apps.CredoapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
