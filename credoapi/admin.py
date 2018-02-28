2a6390163d770660c06fd53d5351f307788ca42c
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-03-01 00:25:05 +0100

    Register credoapi.models.* in admin.py

--- credoapi/admin.py
+++ credoapi/admin.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Detection, Device, Team, User
+
 # Register your models here.
+
+admin.site.register(Detection)
+admin.site.register(Device)
+admin.site.register(Team)
+admin.site.register(User)
