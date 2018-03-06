9955aaaa8decace69e7c70d37fcc025b04d041c4
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-03-06 15:49:15 +0100

    Add basic api view and urls.

---
+++ credoapi/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from credoapi import views
+
+urlpatterns = [
+    url(r'^$', views.handle_detection)
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- credoapi/views.py
+++ credoapi/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from credoapi.models import Team, User, Device, Detection
+from credoapi.serializers import DetectionSerializer
+
 from django.shortcuts import render
 
-# Create your views here.
+
+@api_view(['POST'])
+def handle_detection(request):
+    serializer = DetectionSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response("", status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
