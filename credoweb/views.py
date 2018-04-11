94505ca97f1c93372f2d5fa92d8fc9b7cf580279
koziomek <krzysztof.oziomek@cyfronet.pl>
2018-04-11 10:54:32 +0200

    Display number of detections/users/teams on the main page

--- credoweb/views.py
+++ credoweb/views.py
@@ -16,6 +16,9 @@ def index(request):
     recent_detections = Detection.objects.all().order_by('-timestamp')[:20]
     top_users = User.objects.annotate(detection_count=Count('detection')).order_by('-detection_count')[:5]
     context = {
+        'detections_total': Detection.objects.count(),
+        'users_total': User.objects.count(),
+        'teams_total': Team.objects.count(),
         'recent_detections': [{
             'date': d.timestamp,
             'user': d.user.name,
