48189e3bd39a2ec5d14b5f31a0195b996d4a7cb9
koziomek <krzysztof.oziomek@cyfronet.pl>
2018-04-11 10:22:26 +0200

    Start decoupling data passed to template from models

--- credoweb/views.py
+++ credoweb/views.py
@@ -5,11 +5,26 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.db.models import Count
+
 from credoapi.models import Team, User, Device, Detection
+
+import base64
 
 
 def index(request):
+    recent_detections = Detection.objects.all().order_by('-timestamp')[:20]
+    top_users = User.objects.annotate(detection_count=Count('detection')).order_by('-detection_count')[:5]
     context = {
-        'detections_recent': Detection.objects.all().order_by('-timestamp')[:20]
+        'recent_detections': [{
+            'date': d.timestamp,
+            'user': d.user.name,
+            'team': d.user.team,
+            'img': base64.encodestring(d.frame_content)
+        } for d in recent_detections],
+        'top_users': [{
+            'name': u.name,
+            'detection_count': u.detection_count
+        } for u in top_users]
     }
     return render(request, 'credoweb/index.html', context)
