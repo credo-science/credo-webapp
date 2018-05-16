37ba1ec3d1f6239f5dba3c88874887b69a27f48e
Marek Magryś <m.magrys@cyfronet.pl>
2018-05-16 23:33:25 +0200

    credoweb: exlude teams with empty name from team list

--- credoweb/views.py
+++ credoweb/views.py
@@ -51,8 +51,8 @@ def team_list(request, page=1):
     page = int(page)
     context = cache.get('team_list_{}'.format(page))
     if not context:
-        p = Paginator(Team.objects.annotate(detection_count=Count('detection')).order_by(
-            '-detection_count'), 20).page(page)
+        p = Paginator(Team.objects.annotate(detection_count=Count('detection')).exclude(name__isnull=True).exclude(
+            name__exact='').order_by('-detection_count'), 20).page(page)
         context = {
             'has_next': p.has_next(),
             'has_previous': p.has_previous(),
