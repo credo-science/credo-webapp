df0e9e27d3fd12c67ea1155126e5ff92d902d158
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-05-16 18:23:19 +0200

    Remove def team for super user.

--- credocommon/models.py
+++ credocommon/models.py
@@ -35,8 +35,6 @@ class User(AbstractUser):
     def save(self, *args, **kwargs):
         if not self.display_name:
             self.display_name = self.username
-        if self.is_superuser:
-            self.team = get_default_team()
         super(User, self).save(*args, **kwargs)
 
     def get_full_name(self):
