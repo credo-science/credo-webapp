c4767bfc6c2850081a585bab5a29211ee528f52b
Marek Magryś <m.magrys@cyfronet.pl>
2018-05-16 18:21:17 +0200

    credoweb: add user list page

--- credoweb/urls.py
+++ credoweb/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'user/(?P<username>.{1,50})/$', views.user_page, name='user_page'),
     url(r'user/(?P<username>.{1,50})/(?P<page>\w+)$', views.user_page, name='user_page_paginated'),
+    url(r'user_list/$', views.user_list, name='user_list'),
+    url(r'user_list/(?P<page>\w+)/$', views.user_list, name='user_list'),
     url(r'team/(?P<name>.{0,50})/$', views.team_page, name='team_page'),
     url(r'confirm_email/(?P<token>[a-z0-9]{64})/$', views.confirm_email, name='confirm_email'),
     url('^', include('django.contrib.auth.urls'))
--- credoweb/views.py
+++ credoweb/views.py
@@ -6,10 +6,13 @@ import time
 
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.cache import cache
 
 from credocommon.models import Team, User, Detection
 from credoweb.helpers import get_global_stats, get_recent_detections, get_top_users, get_recent_users,\
     get_user_detections_page
+
 
 
 def index(request):
@@ -22,6 +25,26 @@ def index(request):
     }
     return render(request, 'credoweb/index.html', context)
 
+def user_list(request, page=1):
+    page = int(page)
+    context = cache.get('user_list_{}'.format(page))
+    if not context:
+        p = Paginator(User.objects.annotate(detection_count=Count('detection')).order_by('-detection_count'), 20).page(
+            page)
+        context = {
+            'has_next': p.has_next(),
+            'has_previous': p.has_previous(),
+            'page_next': page + 1,
+            'page_previous': page - 1,
+            'page_number': page,
+            'users': [{
+                'name': u.username,
+                'display_name': u.display_name,
+                'detection_count': u.detection_count
+            } for u in p.object_list],
+        }
+        cache.set('user_list_{}'.format(page), context)
+    return render(request, 'credoweb/user_list.html', context)
 
 def user_page(request, username='', page=1):
     page = int(page)
