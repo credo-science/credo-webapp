db82147c8222e29033d934f031a05c6a2faa4717
koziomek <krzysztof.oziomek@cyfronet.pl>
2018-06-11 10:44:28 +0200

    Add time check to image validation process

--- credocommon/management/commands/filter_detections.py
+++ credocommon/management/commands/filter_detections.py
@@ -3,15 +3,20 @@ from django.core.management.base import BaseCommand
 from credocommon.models import Detection
 from credocommon.helpers import validate_image
 
+
 class Command(BaseCommand):
-    help = 'Add sample data to db'
+    help = 'Validate detections'
 
     def handle(self, *args, **options):
         detections = Detection.objects.all()
 
         for d in detections:
             if (not d.frame_content) or validate_image(d.frame_content):
-                self.stdout.write("Processing image %s" % d.id)
+                self.stdout.write("Hiding detection %s (image validation failed)" % d.id)
+                d.visible = False
+                d.save()
+            if abs(d.time_received - d.timestamp) > 3600 * 24 * 365 * 5:
+                self.stdout.write("Hiding detection %s (invalid date)" % d.id)
                 d.visible = False
                 d.save()
 
