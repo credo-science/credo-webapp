1cb63c2c24e833c4761e5562011e657b311d8f28
koziomek <krzysztof.oziomek@cyfronet.pl>
2018-06-15 09:54:35 +0200

    Update rules

--- credoweb/forms.py
+++ credoweb/forms.py
@@ -9,9 +9,12 @@ class RegistrationForm(forms.Form):
     team = forms.CharField(max_length=50)
     password = forms.CharField(min_length=6, max_length=128, widget=forms.PasswordInput())
     confirm_password = forms.CharField(min_length=6, max_length=128, widget=forms.PasswordInput())
+    accept_rules = forms.BooleanField()
 
     def clean(self):
         cleaned_data = super(RegistrationForm, self).clean()
 
         if not (cleaned_data.get('password') == cleaned_data.get('confirm_password')):
             self.add_error('password_confirm', 'Passwords do not match')
+        if not cleaned_data.get('accept_rules'):
+            self.add_error('accept_rules', 'Please accept the rules')
