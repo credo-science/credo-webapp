d102c389c0357ee49bdee99f5a73eafa99446b0c
koziomek <krzysztof.oziomek@cyfronet.pl>
2018-07-12 15:10:32 +0200

    Add information about Redis to Readme

--- credo/settings.py
+++ credo/settings.py
@@ -99,9 +99,14 @@ DATABASES = {
 }
 
 CACHES = {
-   'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache'
-   }
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+        "KEY_PREFIX": "credo-webapp"
+    }
 }
 
 RQ = {
